eaff3f85cff1eef2ac4a902397feb2becaaa1b4e
ogasawara1223 <vaella2@akane22.sakura.ne.jp>
2018-03-20 20:36:13 +0900

    show a picture

--- main.js
+++ main.js
@@ -1,17 +1,17 @@
 enchant();
 window.onload = function(){
   var game = new Core(375, 667);
-  /*
-  //core.preload('001.jpg');
-  core.onload = function(){
-    /*
+  game.fps = 30;
+
+  game.preload('001.jpg');
+  game.onload = function(){
     var card = new Sprite(453, 640);
     card.image = game.assets['001.jpg'];
     card.x = 0;
     card.y = 0;
-    core.rootScene.addChild(card);
+    card.frame = 0;
+    game.rootScene.addChild(card);
   };
-  */
 
   game.start();
 };
